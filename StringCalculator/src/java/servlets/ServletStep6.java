5c648d8eb0afbfb71cb302792a8fc6005d089109
Kevin Conter <Kevin Conter>
2017-10-17 20:47:59 +0200

    Aggiunta servlet per la gestione del sesto step

---
+++ StringCalculator/src/java/servlets/ServletStep6.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kevin Conter &lt;k dot conter at enginsoft dot com&gt;
+ */
+public class ServletStep6 extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String numbers = request.getParameter("numbers");
+        Integer result;
+        
+        result = add(numbers);
+        
+        request.setAttribute("numbers", numbers);
+        request.setAttribute("result", result);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/result.jsp");
+        rd.forward(request, response);
+    }
+    
+    private int add(String numbers){
+        if(numbers.length() == 0){
+            return 0;
+        }else{
+            // Ricerca della sequenza \\n in quanto il browser esegue automaticamente
+            // il parsing dei caratteri speciali prima dell'invio del form.
+            numbers = numbers.replace("\\n", "\n");
+            String delimiter = ",";
+            if(numbers.startsWith("//")){
+                String firstRow = numbers.substring(0, numbers.indexOf("\n"));
+                delimiter = firstRow.substring(2, firstRow.length());
+                numbers = numbers.substring(numbers.indexOf("\n"));
+            }
+            
+            numbers = numbers.replaceAll("\n|,", delimiter);
+            String nums[] = numbers.split("[\\"+delimiter+"]");
+            
+            Integer res = 0;
+            LinkedList<Integer> negatives = new LinkedList<>();
+            for(int i=0;i<nums.length;i++){
+                Integer n = nums[i].equals("") ? 0 : Integer.valueOf(nums[i]);
+                if(n<0){
+                    negatives.add(n);
+                }
+                if(n > 1000){
+                    n = 0;
+                }
+                res += n;
+            }
+            if(negatives.size()>0){
+                String message = "Negatives not allowed - ";
+                while(!negatives.isEmpty()){
+                    message += negatives.remove()+";";
+                }
+                throw new IllegalArgumentException(message);
+            }
+            return res;
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
