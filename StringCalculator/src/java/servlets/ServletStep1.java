198ed6656138b831aa4d5816adf4f43846865186
Kevin Conter <Kevin Conter>
2017-10-17 16:30:05 +0200

    Creata servlet per la gestione del primo step

--- StringCalculator/src/java/servlets/ServletStep1.java
+++ StringCalculator/src/java/servlets/ServletStep1.java
@@ -7,6 +7,7 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +30,31 @@ public class ServletStep1 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String numbers = request.getParameter("numbers");
+        Integer result;
         
+        result = add(numbers);
+        
+        request.setAttribute("numbers", numbers);
+        request.setAttribute("result", result);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/result.jsp");
+        rd.forward(request, response);
+    }
+    
+    private int add(String numbers){
+        if(numbers.length() == 0){
+            return 0;
+        }else{
+            String nums[] = numbers.split(",");
+            if(nums.length == 1){
+                return new Integer(nums[0]);
+            }else{
+                Integer n1 = new Integer(nums[0]);
+                Integer n2 = new Integer(nums[1]);
+                return n1+n2;
+            }
+        }
     }
 
     /**
